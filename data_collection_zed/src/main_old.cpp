c35eb79dceb11741c84833cc65572a522f38fcc4
Lithium <meetleopauly@yahoo.com>
2017-11-14 15:03:47 +0000

    modified data collection code

--- data_collection_zed/src/main_old.cpp
+++ data_collection_zed/src/main_old.cpp
@@ -7,7 +7,7 @@
 
 /***********************************************************************************************
  ** Data collection program for collecting rgb images and depth images
- ** Press enter when asked to start recording
+ ** z: for starting recording ; f: for ending recording
  ***********************************************************************************************/
 
  // ZED includes
@@ -19,6 +19,10 @@
 // Sample includes
 #include <SaveDepth.hpp>
 
+// includes
+#include <vector>
+
+
 using namespace sl;
 
 cv::Mat slMat2cvMat(Mat& input);
@@ -26,11 +30,10 @@ void printHelp();
 
 int main(int argc, char **argv) {
 
-    // Create a ZED camera object and other camera variables
+    // Create a ZED camera object
     Camera zed;
     int enter=0,count=0;
     std::string filename_rgb,filename_depth;
-    int counter=0;
 
 
     // Set configuration parameters
@@ -38,8 +41,9 @@ int main(int argc, char **argv) {
     init_params.camera_resolution = RESOLUTION_HD720;
     //init_params.depth_mode = DEPTH_MODE_QUALITY;
     //init_params.coordinate_units = UNIT_CENTIMETER;
-    init_params.camera_fps = 10 ;
+    init_params.camera_fps = 30 ;
     //init_params.depth_minimum_distance = 30 ;
+    int f=0;
 
     // Open the camera
     ERROR_CODE err = zed.open(init_params);
@@ -53,54 +57,86 @@ int main(int argc, char **argv) {
     printHelp();
 
     // Set runtime parameters after opening the camera
-    RuntimeParameters runtime_parameters;
-    runtime_parameters.sensing_mode = SENSING_MODE_STANDARD;
-    zed.setCameraSettings(CAMERA_SETTINGS_EXPOSURE, 40, false);
+    //RuntimeParameters runtime_parameters;
+    //runtime_parameters.sensing_mode = SENSING_MODE_FILL;
 
     // Prepare new image size to retrieve half-resolution images
     Resolution image_size = zed.getResolution();
-    int new_width = image_size.width / 2;
-    int new_height = image_size.height / 2;
+    int new_width = image_size.width;
+    int new_height = image_size.height;
+
+    //print(zed.getCameraSettings())
 
     // To share data between sl::Mat and cv::Mat, use slMat2cvMat()
     // Only the headers and pointer to the sl::Mat are copied, not the data itself
-    Mat image_zed(new_width, new_height, MAT_TYPE_8U_C4);
-    cv::Mat image_ocv = slMat2cvMat(image_zed);
+    Mat image_zed;
+    //cv::Mat image_ocv = slMat2cvMat(image_zed);
     Mat depth_image_zed(new_width, new_height, MAT_TYPE_8U_C4);
-    cv::Mat depth_image_ocv = slMat2cvMat(depth_image_zed);
+    //cv::Mat depth_image_ocv = slMat2cvMat(depth_image_zed);
+    //Mat point_cloud;
+    std::vector<cv::Mat> store;
 
     // Loop until 'q' is pressed
     char key = ' ';
-    char start;
-    start=getchar();
-    while (counter != 40) {
+    //zed.setCameraSettings(CAMERA_SETTINGS_EXPOSURE, 30, false);
+    //zed.setCameraSettings(CAMERA_SETTINGS_EXPOSURE, -1, true);
+    while (f != 100) {
 
-        if (zed.grab(runtime_parameters) == SUCCESS) {
+        if (zed.grab() == SUCCESS) {
 
             // Retrieve the left image, depth image in half-resolution
-            zed.retrieveImage(image_zed, VIEW_LEFT, MEM_CPU, new_width, new_height);
-            //zed.retrieveImage(depth_image_zed, VIEW_DEPTH, MEM_CPU, new_width, new_height);
+            //std::cout << zed.getCameraFPS() << '\n';
+            zed.retrieveImage(image_zed, VIEW_LEFT);
+            zed.retrieveImage(image_zed, VIEW_RIGHT);
+            zed.retrieveImage(depth_image_zed, VIEW_DEPTH, MEM_CPU, new_width, new_height);
+
+            // Retrieve the RGBA point cloud in half-resolution
+            // zed.retrieveMeasure(point_cloud, MEASURE_XYZRGBA, MEM_CPU, new_width, new_height);
 
             // Display image and depth using cv:Mat which share sl:Mat data
-            cv::imshow("Image", image_ocv);
+
+            //cv::imshow("Image", image_ocv);
             //cv::imshow("Depth", depth_image_ocv);
 
+            //if (key=='z')
+            //{std::cout << "Entered" << '\n';
+            //enter=1;}
+            //if (key=='f')
+            //{enter=0;}
 
-            std::cout << "Recording....." << '\n';
-            std::stringstream a;
-	          a << count;
-            filename_rgb="./rgb/"+a.str()+"rgb.png";
-            filename_depth="./depth/"+a.str()+"depth.png";
-            cv::imwrite(filename_rgb,image_ocv);
-            //cv::imwrite(filename_depth,depth_image_ocv);
-            count=count+1;
+
+            // For recording
+            //if (enter==1)
+            //{
+              std::cout << "Recording....." << '\n';
+
+              //store.push_back(image_ocv);
+              //std::cout << image_ocv << '\n';
+
+            //}
+
 
             // Handle key event
-            key =cv::waitKey(10); //std::cout <<"Playing"<<key << '\n';
-            counter=counter+1;
+            //key =cv::waitKey(10); //std::cout <<"Playing"<<key << '\n';
+            // std::cout << f << '\n';
+            //f=f+1;
         }
     }
+
+    //cv::imwrite("leo.png",store.pop_back());
     zed.close();
+    for (std::vector<cv::Mat>::iterator it = store.begin() ; it != store.end(); ++it)
+    {
+    std::stringstream a;
+    a << count;
+    filename_rgb="./rgb/"+a.str()+"rgb.png";
+    filename_depth="./depth/"+a.str()+"depth.png";
+    cv::imwrite(filename_rgb,*it);
+
+    std::cout << count << '\n';
+    //cv::imwrite(filename_depth,depth_image_ocv);
+    count=count+1;
+    }
     return 0;
 }
 
@@ -131,6 +167,7 @@ cv::Mat slMat2cvMat(Mat& input) {
 * This function displays help in console
 **/
 void printHelp() {
-    std::cout << " Press 'enter' to start recording" << std::endl;
-
+    std::cout << " Press 'z' to start recording" << std::endl;
+    std::cout << " Press 'f' to end recording" << std::endl;
+    std::cout << " Press 'q' to quite" << std::endl;
 }
